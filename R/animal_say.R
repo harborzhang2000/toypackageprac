1cbe763029041aec8f0df67026725f863bbf0167
Harbor Zhang <harborz2000@gmail.com>
2023-04-16 17:23:15 -0700

    add documentation for animal_say

--- R/animal_say.R
+++ R/animal_say.R
@@ -1,4 +1,13 @@
-
+#' Call an animal and let it speak for you!
+#'
+#'@param number 1 or 2 in our case to specify an animal
+#'@param string a string stating what you want to say
+#'
+#'@export
+#'
+#'@examples
+#'
+#'animal_say(1, "hahaha!!!")
 animal_say <- function(number, string) {
   if (number == 1) {
     return(cowsay::say(string, by = "cat"))
